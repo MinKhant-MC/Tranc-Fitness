227852bfd081e5d268d75cdca9c609ecc0771d21
MinKhant-MC <sysadmin@mochaauthentic.com>
2026-07-11 18:59:05 +0630

    Update config.js

--- gym-system-ssr/config.js
+++ gym-system-ssr/config.js
@@ -2,7 +2,7 @@
   'use strict';
 
   window.GYM_CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbyRlUpca0L6aXCfxYv3s2Vry2OSrZRi1AB-juexD1z9hRWoL1E-5vD9cTM_I2s4T9CC/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbwNd6BCWbv8vvY9vD-b63q8jebfj8moEpTXaDIHkqsgy_g7HE8Wx6cSbTvVuZHk1AYh/exec',
     QR_IMAGE_URL: 'https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=',
     MEMBER_CACHE_TTL_MS: 7 * 24 * 60 * 60 * 1000,
     STORAGE_KEYS: {
